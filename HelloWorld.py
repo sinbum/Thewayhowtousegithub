a5bb1d9c070a9cf1dcd1b26aaa1b031501a1ad18
sinbum <79790395+sinbum@users.noreply.github.com>
2021-03-05 18:50:56 +0900

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,82 @@
+# print("Hello, World!")
+# print(5)
+# print(-10)
+# print(3.14)
+# print(1000)
+# print(5+3)
+# print(2*8)
+# print(3*(3+1))
+# print('balloon')
+# print('butterfly')
+# print('butterfly'*9)
+# print(5 > 10)
+# pritn(5 < 10)
+# print(true)
+# print(false)
+# print(not True)
+# print(not False)
+# print(not (5 > 10))
+
+
+
+#애완동물을 소개해 주세요~
+# animal = "강아지"
+# name = "댕댕이"
+# age = "4"
+# hobby = "산책"
+# is_adult = age >= 3
+
+# print("우리집 " + animal + "의 이름은 " + name + " 예요 ")
+# print(+ name + "는 "+ str(age) + "살이며" + hobby +"을 아주 좋아해요")
+# print(+ name + "는 몇살일까요?" + str(is_adult)
+
+#애완동물을 소개해 주세요~
+# animal = "개"
+# name = "댕댕이"
+# age = "4"
+# hobby = "산책"
+
+
+# print("우리집 " + animal +"의 이름은" + name + "에요")
+# print(name + "는 " + str(age) + "살이며, " + hobby + "을 아주 좋아해요")
+# print(name + "는 어른일까요?")
+
+# # 주석 달기 비주얼 스튜디오 코드
+
+# crtl + /
+
+# print("Hello,World")
+# a = 1
+# b = 2
+# print( b + a )
+# c = False
+
+# kitchen = ("water tab","pot","spoon","cisor","cup","plate")
+
+# print (type(kitchen))
+# print (kitchen)
+
+# def say_hello(name="genius"):
+#   print("hello", name)
+
+
+# say_hello("saetbyeol")
+# say_hello()  
+
+# if 1 + 1 == 2:
+#     print("this is correct")
+# else: 
+#     print("this is not correct")
+
+# def quiz():
+#     answer = input('1 + 2 = 5')
+#     return 1 + 2 == int(answer)
+
+#     quiz()
+
+def say_hello(name, age) :
+    return f"Hello {name} you are {age} years old"
+
+hello = say_hello(age="nico",name="12")
+
+print(hello)
